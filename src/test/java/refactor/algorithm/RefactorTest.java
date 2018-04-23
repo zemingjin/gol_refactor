ce5b6e7f4d7bb1f66a4025a52e8db9e22e039703
mjin <ming.jin@orbitz.com>
2018-04-23 09:13:16 -0500

    Fixed the test.

--- src/test/java/refactor/algorithm/RefactorTest.java
+++ src/test/java/refactor/algorithm/RefactorTest.java
@@ -27,8 +27,8 @@ public class RefactorTest {
         final Refactor refactor = new Refactor(SeedHelper.getLiveCellsMap("1|0, 1|1, 1|2"));
 
         assertEquals(12, refactor.getNeighbouringDeadCells().size());
-        assertEquals("[0|-1, 1|-1, 2|-1, 0|0, 2|0, 0|1, 2|1, 0|2, 2|2, 0|3, 1|3, 2|3]",
-                     refactor.getNeighbouringDeadCells().toString());
+        assertEquals("[0|-1, 0|0, 0|1, 0|2, 0|3, 1|-1, 1|3, 2|-1, 2|0, 2|1, 2|2, 2|3]",
+                     sort(refactor.getNeighbouringDeadCells()).toString());
     }
 
     @Test
